d3ccdd6f0de0cba3404b701c12253051376f937a
unknown <yuhong_cai@hotmail.com>
2023-08-08 09:40:25 -0400

    starlight
    ios_version

---
+++ app/language/EN/language.data.ts
@@ -0,0 +1,43 @@
+const DataList= {
+    "BlueToothConnectScreen":{
+        "Text": [
+            `Connected Decvice: `,
+            `Please search for your panel by clicking on "Find My Starlight Panel"`,
+            "Disconnect",
+            "Find My Starlight Panel",
+        ]
+    },
+    "LedControllerScreen":{
+        "Text":[
+            `Warning`, 
+            `If you choose Yes, this setting is back to default.`,
+            `Cancel`,
+            `Yes`,
+            `Light Modes`,
+            `Not Connected`,
+            `Presets`,
+            `Custom`,
+            `Back to Default`,
+            `Add Custom Mode`,
+            `Edit Custom Mode`,
+            `Create a new name`,
+            `Enter the name, then click Submit\n(Max Length 20)`,
+            `Enter Text`,
+            `Error: the name "{VARIABLE}" already exists`
+        ]
+    },
+    "LedCustomScreen":{
+        "Text":[
+            `Warning`,
+            `If you choose Yes, this setting is deleted.`,
+            `Cancel`,
+            `Yes`,
+            `Error`,
+            `Failed to create the new Star Mode`,
+            `Cancel`,
+            `Brightness`,
+        ]
+    }
+}
+
+export default DataList;
